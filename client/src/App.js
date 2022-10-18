6079ae7aab063826d8cdbe1481d9f3acbc507c6c
Nazif Mahamud <nazif.mahamud1213@gmail.com>
2022-10-18 13:50:38 -0400

    Did ctrl+x/y

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,39 @@
 import './App.css';
-import { React } from 'react'
+import { React, useContext, useCallback, useEffect, useLayoutEffect, useRef } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Banner, ListSelector, PlaylistCards, Statusbar, DeleteListModal, DeleteSongModal, EditSongModal } from './components'
+import { GlobalStoreContext } from './store'
 /*
     This is our application's top-level component.
     
     @author McKilla Gorilla
 */
 const App = () => {
+    const { store } = useContext(GlobalStoreContext);
+    const onKeyPress = (event) => {
+        console.log(`key pressed: ${event.key}`);
+    };
+    const useKeyPress = (callback, node = null) => {
+        const callbackRef = useRef(callback);
+        useLayoutEffect(() => {
+            callbackRef.current = callback;
+        });
+        const handleKeyPress = useCallback((event) => {
+                if (event.ctrlKey && (event.key === 'z' || event.key === 'Z') && !(document.getElementById("delete-list-modal").classList.contains("is-visible")) && !(document.getElementById("delete-song-modal").classList.contains("is-visible")) && !(document.getElementById("edit-song-modal").classList.contains("is-visible"))) {store.undo()}
+                if (event.ctrlKey && (event.key === 'y' || event.key === 'Y') && !(document.getElementById("delete-list-modal").classList.contains("is-visible")) && !(document.getElementById("delete-song-modal").classList.contains("is-visible")) && !(document.getElementById("edit-song-modal").classList.contains("is-visible"))) {store.redo()}
+        }, []);
+
+        useEffect(() => {
+            const targetNode = node ?? document;
+            targetNode &&
+            targetNode.addEventListener("keydown", handleKeyPress);
+            return () =>
+                targetNode &&
+                targetNode.removeEventListener("keydown", handleKeyPress);
+        }, [handleKeyPress, node]);
+    };
+    useKeyPress(onKeyPress);
+
     return (
         <Router>
             <Banner />
